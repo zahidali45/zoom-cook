522d9acc287906f975656175838d30a1c389d095
tsc <mz260959@gmail.com>
2024-08-28 15:02:19 +0500

    update

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,7 +8,7 @@ const Page = () => {
         <Image
           className="w-24 h-24 md:w-48 md:h-auto md:rounded-none rounded-full mx-auto"
           src="/zahid.jpg"
-          alt="Muhammad Zahid"
+          alt="Muhammad-Zahid"
           width="384"
           height="512"
         />
